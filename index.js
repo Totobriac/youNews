6c7a8a48cb9b2269dc206c38c7e497fa1add08e7
your_name <your_name@hotmail.com>
2022-09-12 21:47:11 +0200

    split

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const pretty = require("pretty");
 
 var articList = [];
 
-axios.get("https://www.lefigaro.fr/flash-actu")
-  .then((response) => {
-    var html = response.data;
-    var $ = cheerio.load(html);
+function getSlugUrl() {
+  return new Promise((resolve) => {
+    axios.get("https://www.lefigaro.fr/flash-actu")
+      .then((response) => {
+        var html = response.data;
+        var $ = cheerio.load(html);
 
-    var articles = $(".fig-flash__item");
+        var articles = $(".fig-flash__item");
 
-    articles.each(function (i, el) {
-      var slug = ($(el).find("h2").text()).trim();
-      var link = ($(el).find("a").attr("href"));
+        articles.each(function (i, el) {
+          var slug = ($(el).find("h2").text()).trim();
+          var link = ($(el).find("a").attr("href"));
 
-      articList.push({
-        "slug": slug,
-        "link": link
+          articList.push({
+            "slug": slug,
+            "link": link,
+            "picLink": null,
+          })
+        });
+        resolve();
       })
-    });
-    console.log(articList);
-  })
+    }
+  )
+}
+
+
+async function getPicUrl() {
+  await getSlugUrl();
+  for (let i = 0; i < articList.length; i++) {
+    
+    axios.get(articList[i].link)
+      .then((response) => {        
+        var html = response.data;
+        var $ = cheerio.load(html);        
+        var pic = $(".fig-media__container");
+        console.log(pic.text().split("<img srcset="));
+        
+      })
+  }  
+}
+
+getPicUrl();
